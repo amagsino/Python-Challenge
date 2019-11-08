395731b3f181dd9a2f8e5d0e15087efb0bcb2ba5
Aizelmarie M <55722565+amagsino@users.noreply.github.com>
2019-11-08 11:44:25 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,61 +1,64 @@
-import csv
 import os
+import csv
+
+#list used to store data for months, profits, and change
+months = []
+profits = []
+change_list=[]
+greatest_increase = 0
+greatest_decrease = 0
+change=0
 
 #creates a path for file (check this later)
-my_path = os.path.abspath(os.path.dirname(__file__))
-file = os.path.join(my_path, '..', 'Resources', 'budget_data.csv')
-
-#list used to store data for months and revenue
-months = []
-revenue = []
+file = os.path.join('Resources', 'budget_data.csv')
 
 #csv file is split into lists within reader
-with open(file, 'r') as csvfile:
-    csvread = csv.reader(csvfile)
-    
-    next(csvread, None) 
+with open(file) as csvfile:
+    csvread = csv.reader(csvfile, delimiter=',')
+    next(csvread) 
 
-    #revenue list is converted to integers
+    #profits list is converted to integers
+    #append month and profits to proper columns
     for row in csvread:
         months.append(row[0])
-        revenue.append(int(row[1]))
+        profits.append(int(row[1]))
 
-#counts total months through len
-total_months = len(months)
+#goes through profits
+profits = [int(row) for row in profits]
 
-#sets up variables for greatest increase, greatest decrease and total
-#sets greatest increase and greatest decrease starting at the first revenue entry
-greatest_increase = revenue[0]
-greatest_decrease = revenue[0]
-total = 0
+#for loop through profits indices to find greatest increase and greatest decrease
 
-#for loop through revenue indices to find greatest increase and greatest decrease
-#adds each revenue to total revenue
-for r in range(len(revenue)):
-    if revenue[r] >= greatest_increase:
-        greatest_increase = revenue[r]
-        great_increase_month = months[r]
-    elif revenue[r] <= greatest_decrease:
-        greatest_decrease = revenue[r]
-        great_decrease_month = months[r]
-    total += revenue[r]
+for i in range(len(profits)):
+    if i < len(profits) - 1:
+        change = change + (profits[i + 1] - profits[i])
+        change_list.append(change)
+        change = 0
 
-#calculates average change and round to hundredths decimal place
-average_change = round(total/total_months, 2)
+#adds each profits to total profits
+#calculates average change and round
+average_change = round(sum(change_list) / len(change_list),2)
+greatest_increase = max(change_list)
+greatest_decrease = min(change_list)
+greatest_decrease_month = change_list.index(greatest_decrease)
+greatest_increase_month = change_list.index(greatest_increase)
+
+#set variable for greatest and least month
+greatest_month = months[greatest_increase_month+1]
+least_month = months[greatest_decrease_month+1]
 
 #creates a path for output file called pybank_output
-output_destination = os.path.join('Output','pybank_output.txt')
+output_file = os.path.join('..','Output','pybank_output.txt')
 
-#opens the output in writefile mode and prints the summary
-with open(output_destination, 'w') as writefile:
-    writefile.writelines('Financial Analysis\n')
-    writefile.writelines('----------------------------' + '\n')
-    writefile.writelines('Total Months: ' + str(total_months) + '\n')
-    writefile.writelines('Total: $' + str(total) + '\n')
-    writefile.writelines('Average Change: $' + str(average_change) + '\n')
-    writefile.writelines('Greatest Increase in Profits: ' + great_increase_month + ' ($' + str(greatest_increase) + ')'+ '\n')
-    writefile.writelines('Greatest Decrease in Profits: ' + great_decrease_month + ' ($' + str(greatest_decrease) + ')')
+#opens the output in text mode and prints the summary
+with open(output_file, 'w') as text:
+    text.write('Financial Analysis\n')
+    text.write('----------------------------' + '\n')
+    text.write('Total Months: ' + str(len(months)) + '\n')
+    text.write('Total: $' + str(sum(profits)) + '\n')
+    text.write('Average Change: $' + str((average_change)) + '\n')
+    text.write('Greatest Increase in Profits: ' + greatest_month + ' ($' + str(greatest_increase) + ')'+ '\n')
+    text.write('Greatest Decrease in Profits: ' + least_month + ' ($' + str(greatest_decrease) + ')')
 
 #opens the output file in readfile mode and prints
-with open(output_destination, 'r') as readfile:
+with open(output_file, 'r') as readfile:
     print(readfile.read())
